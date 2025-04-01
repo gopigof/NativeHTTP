c2900178e3664b2005dfbb238a670277f5480b2c
Gopi Krishna <gopigof@gmail.com>
2025-04-01 02:52:09 -0400

    Extract URL path

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	//defer l.Close()
+	defer l.Close()
 
 	conn, err := l.Accept()
 	if err != nil {
@@ -23,10 +24,28 @@ func main() {
 	}
 	defer conn.Close()
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buffer := make([]byte, 32*1024) // usual limit of 32KB per request
+	n, err := conn.Read(buffer)
 	if err != nil {
-		fmt.Println("Error writing response: ", err.Error())
+		fmt.Println("Error reading request: ", err.Error())
 		return
+	}
+
+	request := strings.Split(string(buffer[:n]), "\r\n")
+	requestLine := strings.Split(request[0], " ")
+
+	if requestLine[1] == "/" {
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+			return
+		}
+	} else {
+		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+			return
+		}
 	}
 	fmt.Println("Response sent and server closed!")
 }
